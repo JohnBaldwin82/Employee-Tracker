3340b0f6a4bfc32c1e8836a1f16320936297c33f
John Baldwin <johnbaldwin82@gmail.com>
2023-05-22 14:01:14 -0600

    added function for some selection info

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const inquirer = require("inquirer");
 require("console.table");
 
 const questionPrompt = {
-  viewEmployees: "View All Employees",
+  viewEmployee: "View All Employees",
   viewDepartment: "View employees by department",
-  viewManager: "View Managers employees",
+  viewSupervisor: "View Managers employees",
   addEmployee: "Add employee",
   removeEmployee: "Remove employee",
-  updateRole: "Update role of employee",
-  updateEmployeeManager: "Update manager of employee",
-  viewRoles: "View all roles",
+  updateJob: "Update role of employee",
+  updateEmployeeSupervisor: "Update manager of employee",
+  viewJobs: "View all roles",
   end: "Leave",
 };
 
@@ -34,50 +34,115 @@ function prompt() {
       type: "list",
       message: "Please make a selection",
       Choices: [
-        questionPrompt.viewEmployees,
+        questionPrompt.viewEmployee,
         questionPrompt.viewDepartment,
-        questionPrompt.viewManager,
+        questionPrompt.viewSupervisor,
         questionPrompt.addEmployee,
-        questionPrompt.updateRole,
-        questionPrompt.updateEmployeeManager,
-        questionPrompt.viewRoles,
+        questionPrompt.updateJob,
+        questionPrompt.updateEmployeeSupervisor,
+        questionPrompt.viewJobs,
         questionPrompt.end,
       ],
     })
     .then((answer) => {
       console.log("answer", answer);
       switch (answer.action) {
-        case questionPrompt.viewEmployees:
-          viewEmployees();
+        case questionPrompt.viewEmployee:
+          viewEmployee();
           break;
 
         case questionPrompt.viewDepartment:
           viewDepartment();
           break;
 
-        case questionPrompt.viewManager:
-          viewDepartment();
+        case questionPrompt.viewSupervisor:
+          viewSupervisor();
           break;
 
         case questionPrompt.addEmployee:
-          viewDepartment();
+          addEmployee();
           break;
 
-        case questionPrompt.updateRole:
-          viewDepartment();
+        case questionPrompt.updateJob:
+          updateJob();
           break;
 
-        case questionPrompt.updateEmployeeManager:
-          viewDepartment();
+        case questionPrompt.updateEmployeeSupervisor:
+          updateEmployeeSupervisor();
           break;
 
-        case questionPrompt.viewRoles:
-          viewDepartment();
+        case questionPrompt.viewJobs:
+          viewJobs();
           break;
 
-          case questionPrompt.end:
-            viewDepartment();
-            break;
+        case questionPrompt.end:
+          end();
+          break;
       }
     });
+}
+
+function viewEmployee() {
+  let query = `SELECT employee.id, employee.first_name, employee.last_name, job.title, department.name AS department, job.salary, CONCAT(supervisor.first_name, '', supervisor.last_name) AS 
+    supervisor FROM employee
+    LEFT JOIHN employee supervisor on supervisor.id = employee.supervisor_id
+    INNER JOIN job ON (job.id = employee.job_id)
+    INNER JOIN department ON (department.id = job.department_id)
+    GROUP BY employee.id;`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.log("\n");
+    console.log('VIEW EMPLOYEES');
+    console.log("\n");
+    console.table(res);
+    prompt();
+  });
+}
+
+function viewDepartment() {
+    let query = `SELECT department.name AS department, job.title, employee.id, employee.first_name, employee.last_name
+    FROM employee
+    LEFT JOIN job ON (job.id = employee.job_id)
+    LEFT JOIN department ON (department.id = job.department_id)
+    GROUP BY department.name;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\n");
+        console.log('VIEW DEPARTMENT');
+        console.log("\n");
+        console.table(res);
+        prompt();
+      });
+}
+
+function viewSupervisor() {
+    let query = `SELECT CONCAT(supervisor.first_name, '', supervisor.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, job.title
+    FROM employee
+    LEFT JOIN employee supervisor ON supervisor.id = employee.supervisor.id
+    LEFT JOIN job ON (job.id = employee.job_id && employee.supervisor_id != 'NULL')
+    GROUP BY supervisor;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\n");
+        console.log('VIEW SUPERVISOR');
+        console.log("\n");
+        console.table(res);
+        prompt();
+      });
+}
+
+function viewJobs() {
+    let query = `SELECT job.title, employee.id, employee.first_name, employee.last_name, department.name AS department
+    FROM employee
+    LEFT JOIN job ON (job.id = employee.job_id)
+    LEFT JOIN department ON (department.id - job.department_id)
+    GROUP BY job.title;`;
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("\n");
+        console.log('VIEW SUPERVISOR');
+        console.log("\n");
+        console.table(res);
+        prompt();
+      });
 }
